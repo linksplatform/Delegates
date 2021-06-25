824a964981c7745ce4f402fb5411a4b5bdb27198
uselessgoddess <dodickgod@gmail.com>
2021-06-25 13:30:40 +0300

    [C++] 0.1.0 Moving deductions guides to Delegate.h Removing CreateDelegate

--- cpp/Platform.Delegates/Platform.Delegates.Delegate.h
+++ cpp/Platform.Delegates/Platform.Delegates.Delegate.h
@@ -233,8 +233,14 @@ namespace Platform::Delegates
         std::shared_ptr<MemberMethodBase> memberMethod = nullptr;
     };
 
-    template <typename Any>
-    inline constexpr bool EnableDelegateSubtraction = false;
+    template <typename ReturnType, typename... Args>
+    Delegate(ReturnType(function)(Args...)) -> Delegate<ReturnType(Args...)>;
+
+    template <typename ReturnType, typename... Args>
+    Delegate(std::function<ReturnType(Args...)> function) -> Delegate<ReturnType(Args...)>;
+
+    template <typename Class, typename ReturnType, typename... Args>
+    Delegate(std::shared_ptr<Class> object, ReturnType(Class:: *member)(Args...)) -> Delegate<ReturnType(Args...)>;
 }
 
 #endif
