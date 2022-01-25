573e75d568d9d55a6cfdc210241fae0dd39914f0
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-25 16:50:39 +0600

    Make all  parameters nullable

--- csharp/Platform.Delegates/ReadHandler.cs
+++ csharp/Platform.Delegates/ReadHandler.cs
@@ -2,5 +2,5 @@ using System.Collections.Generic;
 
 namespace Platform.Delegates
 {
-    public delegate TLink ReadHandler<TLink>(IList<TLink>? link);
+    public delegate TLink ReadHandler<TLink>?(IList<TLink>? link);
 }
