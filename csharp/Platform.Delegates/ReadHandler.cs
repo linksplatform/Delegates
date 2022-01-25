7913beb6beb84a8e1ef8d36ad01363d279d9e5dd
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-25 20:31:26 +0600

    Rename  to

--- csharp/Platform.Delegates/ReadHandler.cs
+++ csharp/Platform.Delegates/ReadHandler.cs
@@ -2,5 +2,5 @@ using System.Collections.Generic;
 
 namespace Platform.Delegates
 {
-    public delegate TLink ReadHandler<TLink>?(IList<TLink>? link);
+    public delegate TLinkAddress ReadHandler<TLinkAddress>?(IList<TLinkAddress>? link);
 }
--- csharp/Platform.Delegates/WriteHandler.cs
+++ csharp/Platform.Delegates/WriteHandler.cs
@@ -2,5 +2,5 @@ using System.Collections.Generic;
 
 namespace Platform.Delegates
 {
-    public delegate TLink WriteHandler<TLink>?(IList<TLink>? before, IList<TLink>? after);
+    public delegate TLinkAddress WriteHandler<TLinkAddress>?(IList<TLinkAddress>? before, IList<TLinkAddress>? after);
 }
